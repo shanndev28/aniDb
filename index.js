aaddda035d6ecba72b49833321fff3626b36b07d
shanndev28 <fajarkhairul723@gmail.com>
2024-07-06 00:43:24 +0700

    update

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.set("json spaces", 3)
 app.use(express.json())
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:3000'
+    origin: ['http://localhost:3000', 'https://shann.cloud/']
 
 }))
 
